05763d661820e3c6b777cbb76930226e8d5e283d
XXUCHAN <141641630+XXUCHAN@users.noreply.github.com>
2024-09-11 12:56:27 +0900

    [Silver IV] Title: 스택, Time: 272 ms, Memory: 33164 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,26 @@
+n = int(input())
+order = [ list(input().split()) for _ in range(n) ]
+
+result = list()
+
+for i in range(n):
+    if order[i][0] == "push":
+        result.append(order[i][1])
+    elif order[i][0] == "size":
+        print(len(result))
+    elif order[i][0] == "empty":
+        if len(result) == 0 :
+            print("1")
+        else:
+            print("0")
+    elif order[i][0] == "pop":
+        if len(result) == 0:
+            print("-1")
+        else:
+            print(result.pop(-1))
+    elif order[i][0] == "top":
+        if len(result) == 0:
+            print("-1")
+        else:
+            print(result[-1])
+
